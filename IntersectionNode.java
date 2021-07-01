185549390641f977619c2eccd2392c02c8ba7b6f
Uma mahesh <umamaheshnov9@gmail.com>
2021-06-30 19:17:08 -0500

    Create IntersectionNode.java

---
+++ IntersectionNode.java
@@ -0,0 +1,20 @@
+// TC : O(N)
+// SC : O(1)
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> s = new HashSet<>();
+        while(headA != null){
+            s.add(headA);
+            headA = headA.next;
+        }
+        
+        while( headB != null){
+            if(s.contains(headB)){
+                return headB;
+            }
+            headB = headB.next;
+        }
+        return null;
+    }
+}
